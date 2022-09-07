bcabe33dc41a944fac30913bfac9fb5a8590f28d
unknown <cacho.muckey@gmail.com>
2022-09-07 14:18:14 +0800

    try contact form

--- forms/contact.php
+++ forms/contact.php
@@ -9,8 +9,8 @@ if ( isset ($_POST['submit'])) {
   $headers = "From: ".$mailFrom;
   $txt = "You have received an e-mail from " .$name.".\n\n".$message;
 
-  mail ($mailTo, $subject, $txt, $headers );
-  header ("Location: index.php?mailsend ");
+  mail($mailTo, $subject, $txt, $headers );
+  header("Location: index.php?mailsend ");
 
  
 
